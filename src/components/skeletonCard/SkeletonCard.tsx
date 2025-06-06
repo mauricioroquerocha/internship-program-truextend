370f5b8d5273a6bc2bd3344c8f974a511fad9dc4
mauricioroquerocha <mauricio.tomas.roque.rocha@gmail.com>
2025-06-06 14:44:05 -0400

    implement card list component with skeleton loading

---
+++ src/components/skeletonCard/SkeletonCard.tsx
@@ -0,0 +1,21 @@
+import "./SkeletonCard.css"; // or use CSS modules if preferred
+
+export default function SkeletonCard() {
+  return (
+    <div className="skeleton-card">
+      <div className="skeleton-image" />
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          marginLeft: "0.5rem",
+          width: "70%",
+        }}
+      >
+        <div className="skeleton-text title" />
+        <div className="skeleton-text subtitle" />
+        <div className="skeleton-text price" />
+      </div>
+    </div>
+  );
+}
