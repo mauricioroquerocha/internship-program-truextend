d84e06d17267473f2a534e68060633a4bc302db3
mauricioroquerocha <mauricio.tomas.roque.rocha@gmail.com>
2025-06-06 14:55:56 -0400

    implement unit test for card list when search

---
+++ __mocks__/multimediaSampleData.ts
@@ -0,0 +1,18 @@
+import { Multimedia } from "../src/types/Multimedia";
+
+export const sampleMultimediaList: Multimedia[] = [
+  {
+    trackId: 1,
+    trackName: "Song One",
+    artistName: "Artist One",
+    trackPrice: 1.99,
+    artworkUrl100: "https://example.com/image1.jpg",
+  },
+  {
+    trackId: 2,
+    trackName: "Song Two",
+    artistName: "Artist Two",
+    trackPrice: 2.99,
+    artworkUrl100: "https://example.com/image2.jpg",
+  },
+];
